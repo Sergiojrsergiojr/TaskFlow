6c94596eca6cd348e39acf00d0323327cc32417b
Sergiojrsergiojr <sergioerbetico@gmail.com>
2025-10-01 12:40:11 +0200

    Dinamizando TaskFlow

--- public/index.php
+++ public/index.php
@@ -1,1 +1,28 @@
+<?php
+define("SITE_NAME","TaskFlow");
+$pageTitle=SITE_NAME." - Página de Inicio";
+$userName = "Sergio"; // Tipo String
+$userAge = 21;             // Tipo Integer
+$isPremiumUser = true;     // Tipo Boolean
 
+// Bloque de configuración principal
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $pageTitle?></title>
+</head>
+<body>
+    <header>
+        <h1>Bienvenido a <?php echo SITE_NAME; ?></h1>
+    </header>
+
+    <main>
+        <h2>Perfil del Usuario</h2>
+        <p><strong>Nombre:</strong> <?php echo $userName; ?></p>
+        <p><strong>Edad:</strong> <?php echo $userAge; ?> años</p>
+        <p><strong>Estado de la cuenta:</strong> Usuario <?php echo $isPremiumUser ? "Premium" : "Estándar"; ?></p>
+    </main>
+</body>
+</html>
