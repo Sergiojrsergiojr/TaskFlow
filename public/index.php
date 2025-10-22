b40c6a1f7f7ef8de4b092c1f077ce4a3dc3fbfad
Sergiojrsergiojr <sergioerbetico@gmail.com>
2025-10-22 13:03:12 +0200

    Primer commit de header.php y footer.php

--- public/index.php
+++ public/index.php
@@ -6,23 +6,11 @@ $userAge = 21;             // Tipo Integer
 $isPremiumUser = true;     // Tipo Boolean
 
 // Bloque de configuración principal
-?>
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <title><?php echo $pageTitle?></title>
-</head>
-<body>
-    <header>
-        <h1>Bienvenido a <?php echo SITE_NAME; ?></h1>
-    </header>
 
-    <main>
+include "/../app/views/header.php"
+
         <h2>Perfil del Usuario</h2>
         <p><strong>Nombre:</strong> <?php echo $userName; ?></p>
         <p><strong>Edad:</strong> <?php echo $userAge; ?> años</p>
         <p><strong>Estado de la cuenta:</strong> Usuario <?php echo $isPremiumUser ? "Premium" : "Estándar"; ?></p>
-    </main>
-</body>
-</html>
+include "/../app/views/footer.php" 
