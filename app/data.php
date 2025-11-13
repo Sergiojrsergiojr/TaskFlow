ab3c691afbb558ab3e1016dfa56ff46d0b1271d1
Sergiojrsergiojr <sergioerbetico@gmail.com>
2025-11-13 10:39:06 +0100

    Actualizando para mejorar el phpUnit

--- app/data.php
+++ app/data.php
@@ -1,15 +1,15 @@
 <?php
 // app/data.php
 $usuarios_bbdd = [
-'usuario1@taskflow.com' => [
-'id' => 1,
-'nombre' => 'Sergio Jimenez',
-'password' => '1234' // Simulación de contraseña (sin hashear, por ahora)
-],
-'usuario2@taskflow.com' => [
-'id' => 2,
-'nombre' => 'Abel Gamez',
-'password' => '1234'
-]
+    'usuario1@taskflow.com' => [
+        'id' => 1,
+        'nombre' => 'Sergio Jimenez',
+        'password' => '1234' // Simulación de contraseña (sin hashear, por ahora)
+    ],
+    'usuario2@taskflow.com' => [
+        'id' => 2,
+        'nombre' => 'Abel Gamez',
+        'password' => '1234'
+    ]
 ];
 ?>
