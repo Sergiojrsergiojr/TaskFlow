2c2e3b1232dfb9e56a9e26404be33636eb8709e1
Sergiojrsergiojr <sergioerbetico@gmail.com>
2025-11-14 11:28:47 +0100

    P4:Arquitectura,Autenticacion y Pruebas en Taskflow

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -3,15 +3,21 @@
 session_start();
 function handleLogin($email, $password, $usuarios_bbdd)
 {
-    if (
-        isset($usuarios_bbdd[$email]) && $usuarios_bbdd[$email]['password']
-        === $password
-    ) {
+    if (!isset($usuarios_bbdd[$email])) {
+        echo "Usuario no encontrado: $email\n";
+        return false;
+    }
+
+    if ($usuarios_bbdd[$email]['password'] === $password) {
         $_SESSION['user_id'] = $usuarios_bbdd[$email]['id'];
         $_SESSION['user_nombre'] = $usuarios_bbdd[$email]['nombre'];
         return true;
+    } else {
+        echo "Contraseña incorrecta para $email\n";
+        var_dump($usuarios_bbdd[$email]['password']);
+        var_dump($password);
+        return false;
     }
-    return false;
 }
 function handleLogout()
 {
--- public/tests/AuthTest.php
+++ public/tests/AuthTest.php
@@ -11,6 +11,20 @@ class AuthTest extends TestCase
     // Prueba para el "camino feliz" (login correcto)
     public function testLoginConCredencialesValidas()
     {
+
+        $usuarios_bbdd = [
+            'usuario1@taskflow.com' => [
+                'id' => 1,
+                'nombre' => 'Sergio Jimenez',
+                'password' => '1234' // Simulación de contraseña (sin hashear, por ahora)
+            ],
+            'usuario2@taskflow.com' => [
+                'id' => 2,
+                'nombre' => 'Abel Gamez',
+                'password' => '1234'
+            ]
+        ];
+
         global $usuarios_bbdd;
 
         // Actuar: llamamos a la función con usuario válido
@@ -23,6 +37,19 @@ class AuthTest extends TestCase
     // Prueba para el "camino triste" (login incorrecto)
     public function testLoginConCredencialesInvalidas()
     {
+        $usuarios_bbdd = [
+            'usuario1@taskflow.com' => [
+                'id' => 1,
+                'nombre' => 'Sergio Jimenez',
+                'password' => '1234' // Simulación de contraseña (sin hashear, por ahora)
+            ],
+            'usuario2@taskflow.com' => [
+                'id' => 2,
+                'nombre' => 'Abel Gamez',
+                'password' => '1234'
+            ]
+        ];
+
         global $usuarios_bbdd;
 
         // Actuar: llamamos a la función con contraseña errónea
